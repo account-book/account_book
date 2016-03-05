7854b0303dd43465f5f251be1d196e815985e840
Yuuto Hasegawa <hsgwyut1997@outlook.jp>
2016-03-05 07:02:03 +0000

    総資産が出るようにしました

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def top
+  	@remain = current_user.transfers.pluck(:amount).inject(:+).to_i + current_user.init_property
   end
 end
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,2 +1,4 @@
 class Transfer < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :transfer_group
 end
